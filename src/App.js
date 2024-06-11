e9c581f4c8ba2146e1f4908cd3af004c65e69464
taner <tanerozer16@gmail.com>
2024-06-11 15:05:27 +0300

    Appjs added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Header from "./components/Header.js";
+import Money from "./components/Money.js";
+import Items from "./components/Items.js";
+import Invoice from "./components/Invoice.js";
+import Footer from "./components/Footer.js";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { cart } = useSelector((state) => state.player);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="root_container">
+        <Header />
+        <Money />
+        <Items />
+        {cart.length > 0 ? <Invoice /> : <p>Your shopping cart is empty.</p>}
+      </div>
+      <Footer />
+    </>
   );
 }
 
