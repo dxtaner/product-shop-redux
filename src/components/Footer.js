2ca0f519412d065bbedce3645408deadebbeef3f
taner <tanerozer16@gmail.com>
2024-06-12 14:25:48 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>
+          2023 || Created by{" "}
+          <a
+            href="https://github.com/dxtaner"
+            target="_blank"
+            rel="noopener noreferrer">
+            dxtaner
+          </a>{" "}
+          ❤️
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
